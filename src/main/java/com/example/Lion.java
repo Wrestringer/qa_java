85b011aac3885e3145ab321d4a2afc91a668379c
Danil Martyushev <martyusheff.danil@yandex.ru>
2024-06-19 23:12:22 +0300

    class Lion extends from class Feline

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -2,24 +2,24 @@ package com.example;
 
 import java.util.List;
 
-public class Lion {
+public class Lion extends Feline {
 
     boolean hasMane;
 
     public Lion(String sex) throws Exception {
+
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
 
-    Feline feline = new Feline();
 
     public int getKittens() {
-        return feline.getKittens();
+        return super.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -27,6 +27,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return super.getFood("Хищник");
     }
 }
